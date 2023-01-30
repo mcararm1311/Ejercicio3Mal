196639c60635ece88bd56f72891595280ced7b59
mcararm1311 <mcararm1311@g.educaand.es>
2023-01-30 13:42:59 +0100

    Initial commit

---
+++ librerias-master/src/YoEjercicio3/Ejercicio3/Ejercicio3.java
@@ -0,0 +1,130 @@
+package YoEjercicio3.Ejercicio3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ejercicio3 {
+private static final Scanner SCIN= new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+
+        List<String> nombres=new ArrayList<>();
+            Scanner sc = new Scanner(new File("Fichero/nombres.txt"));
+
+
+        System.out.println("Continuación de programa");
+
+
+
+        while (sc.hasNextLine()){
+            String nombre = sc.nextLine();
+            nombres.add(nombre.toLowerCase());
+        }
+        System.out.printf("Lista con %d nombres %s%n",nombres.size() ,nombres);
+        System.out.printf("%n----------------------------------------------------------------%n");
+        List<String> nombress =obtenerNombreAcabanConsonante(nombres);
+        /*System.out.printf(" Lista de nombres que acaban en consonante %s%n",nombress);*/
+        mostrarNombresDeLaListaAcabadosConsonantes(nombress);
+
+        String letraa="";
+      do {
+          System.out.println("Dime la primera letra con la que empieza");
+          letraa = SCIN.nextLine();
+      }while( ! letraa.toLowerCase().matches("[a-záéíóúñ]+"));
+
+        List<String> nombreComienzaLetra = obtenerListaEmpizaPorLetra(nombres,letraa);
+
+        System.out.println(nombreComienzaLetra);
+//----------------------------------------------------------------------------------------------------//
+
+        System.out.println("Dime una palabra para comprobar si la contiene algun nombre del fichero");
+        String palabra = sc.nextLine();
+
+
+        boolean existe = existePalabra(nombres,palabra);
+
+        if (existe ){
+            System.out.printf(" %s, Sí se encuentra en nombres del fichero %n", palabra);
+        }else {
+            System.out.printf("%s, No se encuentra en nombres del fichero %n" , palabra);
+        }
+
+        System.out.println();
+
+        //------------------------------------------------------------------------------------//
+
+        System.out.println("Dime otra palabra para buscar y devolverte la lista de los nombres");
+        String palabra2 = sc.nextLine();
+
+
+        List<String>listaNombresP = obtenerListaQueContenganPalabra(nombres,palabra2);
+
+        System.out.println(listaNombresP.toString());
+
+        sc.close();
+
+    }
+
+    private static List<String> obtenerNombreAcabanConsonante(List<String> listas){
+        List<String> nombress=new ArrayList<>();
+
+        for (String lista:listas ) {
+
+            if (lista.toLowerCase().matches(".*[^aeiouáéíóú]")){
+                nombress.add(lista);
+            }
+
+        }
+        /*System.out.println(nombress);*/
+
+        return nombress;
+    }
+
+    private static void mostrarNombresDeLaListaAcabadosConsonantes( List<String> nombress){
+        for (String nom:nombress ) {
+            System.out.println(nom);
+
+        }
+    }
+
+    private static List<String> obtenerListaEmpizaPorLetra(List<String> listas, String letraa){
+
+        List<String> listaEmpiezaLetra= new ArrayList<>();
+
+        for (String lista:listas ) {
+
+            if (lista.toLowerCase().startsWith(letraa.toLowerCase())){
+                listaEmpiezaLetra.add(lista);
+            }
+
+        }
+
+
+        return listaEmpiezaLetra;
+    }
+
+
+    private static boolean existePalabra(List<String> lista , String palabra){
+
+        return lista.contains(palabra.toLowerCase());
+    }
+
+    private static List<String> obtenerListaQueContenganPalabra(List<String> lista , String palabra2){
+
+        List<String> contienenPalabra = new ArrayList<>();
+
+        for (String list:lista ) {
+
+            if (lista.contains(palabra2.toLowerCase())){
+                contienenPalabra.add(lista.toString());
+            }
+        }
+
+
+
+        return contienenPalabra;
+    }
+
+}
---
+++ librerias-master/src/strings/StringHelper.java
@@ -0,0 +1,103 @@
+package strings;
+
+import java.util.Random;
+
+/**
+ * HelpeClass that offers helper methods
+ * related with strings
+ * @author Martin
+ * @version 1.0.0
+ */
+public class StringHelper {
+
+    private  StringHelper(){}
+    /**
+     * Method that returns a boolean if the password contains at least
+     * a digit, a lowercase letter, an uppercase letter and a symbol,
+     * besides the length of the password has to be between minLen and maxLen
+     * @param minLen minimum length
+     * @param maxLen maximum length
+     * @param password password to check
+     * @return true if the password has the conditions, otherwise false
+     */
+      public static boolean isSafePassword(int minLen, int maxLen, String password){
+        if (maxLen < minLen)
+            return false;
+        if (! (password.length() >= minLen && password.length() <= maxLen))
+            return false;
+        if (! password.matches(".*[a-záéíóúüñ].*"))
+            return false;
+        if (! password.matches(".*[A-ZÁÉÍÓÚÜÑ].*"))
+            return false;
+        //if (! password.matches(".*[\\d].*"))
+        if (! password.matches(".*[0-9].*"))
+            return false;
+        if (! password.matches(".*[^a-zA-ZáéíóúüÁÉÍÓÚÜñÑ0-9].*"))
+            return false;
+        return true;
+    }
+
+    /**
+     * Method that returns a boolean if the password contains at least
+     *  a digit, a lowercase letter, an uppercase letter and a symbol,
+     *  besides the length of the password has to be between 8 and 12,
+     *  values included
+     * @param password password to check
+     * @return true if the password has the conditions, otherwise false
+     */
+    public static boolean isSafePasswordDefault(String password) {
+        return isSafePassword(8, 12, password);
+    }
+
+    //FALTA DOCUMENTAR
+    public static boolean checkPhoneNumber(String number) {
+        return number.matches("[679][0-9]{8}");
+    }
+
+    //FALTA DOCUMENTAR
+    public static boolean isValideID(String id) {
+        id = id.toLowerCase();
+        if (! id.matches("[0-9]{8}[a-hj-np-tv-z]")) {
+            return false;
+        }
+        char letraID = id.charAt(id.length() - 1);
+        String sNumero = id.substring(0 , id.length() - 1);
+        int iNumero = Integer.parseInt(sNumero);
+        int resto = iNumero % 23;
+        //char[] datos = {'t', 'r', ...};
+        String datos = "trwagmyfpdxbnjzsqvhlcke";
+        char letraDatos = datos.charAt(resto);
+        return letraDatos == letraID;
+    }
+    public static String createSafePassword (int length){
+        if (length < 4)
+            return null;
+      //  int contador = 0;
+        Random random = new Random();
+        final String  FUENTE_CARACTERES =
+            "abcdefghijklmnñopqrstuvwxyzABCDEFEGHIJKLMNÑOPQRSTUVWXYZ¿?()=@.:,;!¡&{}012456789";
+        StringBuilder stringBuilder = null;
+        do {
+            stringBuilder = new StringBuilder();
+            for (int i = 0; i < length; i++) {
+                int posicion = random.nextInt(FUENTE_CARACTERES.length());
+                char caracter = FUENTE_CARACTERES.charAt(posicion);
+                stringBuilder.append(caracter);
+            }
+        //    System.out.println("Intento: " + contador++);
+        } while ( ! isSafePassword(length, length, stringBuilder.toString()));
+        return stringBuilder.toString();
+
+
+    }
+    public static void main(String[] args) {
+        //System.out.println(isSafePassword(12, 12, "1aÁabc$Nñg"));
+        // System.out.println(isSafePasswordDefault("1aÁabc_Nñg"));
+        //System.out.println(checkPhoneNumber("723456789"));
+       // System.out.println(isValideID("00123456S".replaceAll("[- ]", "")));
+      /*  for (int i = 0; i < 10 ; i++) {
+            System.out.println(createSafePassword(i));
+        }*/
+        System.out.println(createSafePassword(4));
+    }
+}
